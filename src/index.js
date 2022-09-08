bb489d5621ebbe143aed93f02c64c242551ac7b3
wjdghks963 <chsw000@naver.com>
2022-09-08 18:15:19 +0900

    기능 구현 2차 과제

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let AllToDos = new Array();
+
 /**
  * @constructor
  * @param {number} id
@@ -6,7 +8,15 @@
  * @param {string} category
  * @param {Array<string?>} tags
  */
-function ToDo(id, content, isDone, category, tags) {}
+function ToDo(id, content, isDone, category, tags) {
+  const ToDo = new Map();
+  ToDo.set("id", id);
+  ToDo.set("content", content);
+  ToDo.set("isDone", isDone);
+  ToDo.set("category", category);
+  ToDo.set("tags", tags);
+  return ToDo;
+}
 
 /** create */
 /**
@@ -16,45 +26,66 @@ function ToDo(id, content, isDone, category, tags) {}
  * @param {boolean} isDone
  * @param {string} category
  * @param {Array<string?>} tags
- * @return {object}
+ * @return {void}
  */
 
-ToDo.prototype.createTodo = function (id, content, isDone, category, tags) {};
+ToDo.prototype.createToDo = function (id, content, isDone, category, tags) {
+  const newToDo = ToDo(id, content, isDone, category, tags);
+  return AllToDos.push(newToDo);
+};
 
 /** Read */
 /**
  * @return {void}
- * @return {Array<object?>?}
+ * @return {Array<ToDo?>?}
  */
-ToDo.prototype.readAll = function () {};
+ToDo.prototype.readAll = function () {
+  return AllToDos;
+};
 /**
  *
  * @param {number} id
- * @return {object}
+ * @return {ToDo?}
  */
-ToDo.prototype.findById = function (id) {};
+ToDo.prototype.findById = function (id) {
+  return AllToDos.filter((i) => {
+    if (i.get("id") === id) {
+      return i;
+    } else {
+      null;
+    }
+  });
+};
 
 /** Update */
 /**
- *
- * @param {string} content : ;
+ * @param {ToDo} ToDo
+ * @param {string} content
  * @param {boolean} isDone
  * @param {string} category
  * @param {Array<string?>} tags
  * @return {void}
  */
 ToDo.prototype.updateAllExceptId = function (
+  ToDo,
   content,
   isDone,
   category,
   tags
-) {};
+) {
+  ToDo.set("content", content);
+  ToDo.set("isDone", isDone);
+  ToDo.set("category", category);
+  ToDo.set("tags", tags);
+};
 /**
- *
+ * @param {ToDo} ToDo
  * @param {Array<string?>} tags
  * @return {void}
  */
-ToDo.prototype.updateTags = function (tags) {};
+ToDo.prototype.updateTags = function (ToDo, tags) {
+  ToDo.set("tags", tags);
+};
 
 /** Delete */
 /**
@@ -62,18 +93,33 @@ ToDo.prototype.updateTags = function (tags) {};
  * @param {number} id
  * @return {void}
  */
-ToDo.prototype.deleteById = function (id) {};
+ToDo.prototype.deleteById = function (id) {
+  const ToDo = ToDo.prototype.findById(id);
+  return (AllToDos = AllToDos.fillter((i) => {
+    i !== ToDo;
+  }));
+};
 /**
  * @return {void}
  */
-ToDo.prototype.deleteAll = function () {};
+ToDo.prototype.deleteAll = function () {
+  return (AllToDos = new Array());
+};
 /**
- *
+ * @param {ToDo} ToDo
  * @param {string} tagName
  * @return {void}
  */
-ToDo.prototype.deleteTagByName = function (tagName) {};
+ToDo.prototype.deleteTagByName = function (ToDo, tagName) {
+  const tags = ToDo.get("tags");
+  const lefttags = tags.fillter((i) => i !== tagName);
+  return ToDo.set("tags", lefttags);
+};
+
 /**
+ * @param {ToDo} ToDo
  * @return {void}
  */
-ToDo.prototype.deleteAllTags = function () {};
+ToDo.prototype.deleteAllTags = function (ToDo) {
+  return ToDo.set("tags", []);
+};
